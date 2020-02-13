44d8d4a3caec69be002f1c057b2b47da9faf6f06
Oliver Collins <Oliver.Collins@Colorado.edu>
2020-02-13 00:43:27 -0500

    Update TXTtoCSV.py

--- src/TXTtoCSV.py
+++ src/TXTtoCSV.py
@@ -1,5 +1,14 @@
+# Need to change these variables
+input_file = None
+output_file = None
+num_columns = None
+
+# Make sure user changes input variables
+if num_columns == None or input_file == None or output_file == None:
+    return "Please change input variables"
+
 # Open the txt file
-file = open("INPUT.txt", 'r', encoding = "utf-8")
+file = open(input_file, 'r', encoding = "utf-8")
 
 # Read the file
 new_text = file.readlines()
@@ -10,17 +19,17 @@ line_break = 0
 
 # Add all the words in file to list
 for x in range(0, len(new_text)):
-	for word in new_text[x].split():
-		words.append(word + ',')
+    for word in new_text[x].split():
+        words.append(word + ',')
 
 # Write words into csv file
-f = open('OUTPUT.csv','w')
+f = open(output_file,'w')
 for x in range(0, len(words)):
-	if (line_break == NUMBER OF ATTRIBUTES):
-		f.write('\n')
-		f.write(words[x])
-		line_break = 1
-	else:
-		f.write(words[x])
-		line_break += 1
+    if (line_break == num_columns):
+        f.write('\n')
+        f.write(words[x])
+        line_break = 1
+    else:
+        f.write(words[x])
+        line_break += 1
 f.close()
